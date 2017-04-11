295b932e88ae5815aad066d97481135dbb7b8e9b
Yang Yu <yangyangloveyy@126.com>
2017-04-11 19:53:37 +0800

    任务信息，运输轨迹，共享货跟

---
+++ app56kongpsPro/nodePro.py
@@ -0,0 +1,56 @@
+# usr/bin/python
+# encoding:utf-8
+# 足迹版测试--执行定检流程测试
+import unittest
+from time import sleep
+from method56kongps import pickupDelivery
+from method import setParam56kongps
+from method56kongps import taskInfo
+from method56kongps import trackInfo
+from method56kongps import uploadPic
+from method56kongps import share
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = setParam56kongps.setParam(self)
+
+
+    def testNode(self):
+        try:
+            '''从首页定检按钮进入'''
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/iv_node").click()
+            sleep(3)
+
+            #联系人信息页
+            self.driver.find_elements_by_id("com.yihu001.kon.driver:id/iv_pickup_icon")[0].click()
+            #截图
+            self.driver.get_screenshot_as_file("contactFromNode.png")
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/iv_back").click()
+
+
+            #点出菜单
+            self.driver.find_element_by_class_name("android.widget.LinearLayout").click()
+
+            '''菜单操作项'''
+            #任务详情
+
+            #运输轨迹
+
+            #执行定检
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_event").click()
+            count = self.driver.find_elements_by_id("com.yihu001.kon.driver:id/btn_execute").count()
+            print(count)
+
+            #共享货跟
+
+
+        except Exception as e:
+            print(e)
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
