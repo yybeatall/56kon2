7bce8da2fca21ece92849bca2bbb72fbd37be67f
Yang Yu <yangyangloveyy@126.com>
2017-04-11 19:54:18 +0800

    定检

--- app56kongpsPro/handoverPro.py
+++ app56kongpsPro/handoverPro.py
@@ -9,6 +9,7 @@ from method56kongps import taskInfo
 from method56kongps import trackInfo
 from method56kongps import uploadPic
 from method56kongps import share
+
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.driver = setParam56kongps.setParam(self)
@@ -19,30 +20,32 @@ class MyTestCase(unittest.TestCase):
             '''从首页交接按钮进入'''
             self.driver.find_element_by_id("com.yihu001.kon.driver:id/iv_handover").click()
             sleep(3)
+
+            '''批量提到货'''
             # #提货交接
             # pickupDelivery.pickup(self)
             # sleep(3)
             # #到货交接
             # pickupDelivery.delivery(self)
 
-            #选择提货任务
+            '''单个任务操作--提货'''
             self.driver.find_element_by_xpath( '//android.widget.TextView[@text="提"]' ).click( )
             self.driver.find_element_by_class_name("android.widget.LinearLayout").click()
 
             '''菜单操作项'''
             # 任务详情
-            #self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_detail").click()
-            #taskInfo.taskInfo(self,"pickup")
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_detail").click()
+            taskInfo.taskInfo(self,"pickup")
 
-            # #提货交接
-            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_handover").click()
-            # #确认提货
-            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
-            # self.driver.find_element_by_class_name("android.widget.ImageButton").click()
+            #提货交接
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_handover").click()
+            #确认提货
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
+            self.driver.find_element_by_class_name("android.widget.ImageButton").click()
 
             # #拒绝任务
-            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_refuse").click()
-            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_ok").click()
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_refuse").click()
+            self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_ok").click()
 
             #上传照片
             self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_picture").click()
@@ -86,6 +89,72 @@ class MyTestCase(unittest.TestCase):
             self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_cancel").click()
             self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
             self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_ok").click()
+            sleep(3)
+            self.driver.find_element_by_id("android.widget.ImageButton").click()
+
+            '''单个任务操作--到货'''
+            #self.driver.find_element_by_xpath( '//android.widget.TextView[@text="提"]' ).click( )
+            #self.driver.find_element_by_class_name("android.widget.LinearLayout").click()
+
+            '''菜单操作项'''
+            # 任务详情
+            #self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_detail").click()
+            #taskInfo.taskInfo(self,"pickup")
+
+            # #提货交接
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_handover").click()
+            # #确认提货
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
+            # self.driver.find_element_by_class_name("android.widget.ImageButton").click()
+
+            # #拒绝任务
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_refuse").click()
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_ok").click()
+
+            #上传照片
+            #self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_picture").click()
+            #uploadPic.uploadPicFirst(self,3)
+
+            #返回
+            #self.driver.find_element_by_id("android.widget.ImageButton").click()
+
+            #共享货跟
+            #self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_share").click()
+            #share.shareToContacts(self)
+
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_share").click()
+            # share.shareToGroup(self)
+            #
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_share").click()
+            # share.shareToPhone(self)
+            #
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_share").click()
+            # share.shareToAddrList(self)
+            #
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_share").click()
+            # share.cancle(self)
+
+            #批量操作任务
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/toolbar_right").click()
+            # self.driver.find_element_by_xpath(
+            #     '//android.support.v7.widget.RecyclerView/android.widget.LinearLayout[@index=0]').click()
+            # self.driver.find_element_by_xpath(
+            #     '//android.support.v7.widget.RecyclerView/android.widget.LinearLayout[@index=1]' ).click( )
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/share").click()
+            # share.shareToContacts( self )
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/share").click()
+            # share.shareToGroup( self )
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/share").click()
+            # share.shareToPhone( self )
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/share").click()
+            # share.cancle( self )
+
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_cancel").click()
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/handover").click()
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/btn_ok").click()
+            # sleep(3)
+            # self.driver.find_element_by_id("android.widget.ImageButton").click()
 
 
             #选择到货任务
@@ -115,24 +184,24 @@ class MyTestCase(unittest.TestCase):
 
 
 
-            '''到货的上传照片'''
-            # 上传照片
-            self.driver.find_element_by_id( "com.yihu001.kon.driver:id/tv_task_picture" ).click( )
-            uploadPic.uploadPicFirst( self, 3 )
-
-            # 向下滑动
-            e1 = self.driver.find_elements_by_xpath( '//android.widget.TextView[contains(@text,"到货")]' )
-            e2 = self.driver.find_elements_by_xpath( '//android.widget.TextView[contains(@text,"车况")]' )
-            self.driver.drag_and_drop( e1, e2 )
-
-            uploadPic.uploadpicAgain( self, 2 )
-
-            #运输轨迹
-            self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_track").click()
-            sleep(2)
-            # 截图
-            self.driver.get_screenshot_as_file( "track.png" )
-            trackInfo.trackInfo(self)
+            # '''到货的上传照片'''
+            # # 上传照片
+            # self.driver.find_element_by_id( "com.yihu001.kon.driver:id/tv_task_picture" ).click( )
+            # uploadPic.uploadPicFirst( self, 3 )
+            #
+            # # 向下滑动
+            # e1 = self.driver.find_elements_by_xpath( '//android.widget.TextView[contains(@text,"到货")]' )
+            # e2 = self.driver.find_elements_by_xpath( '//android.widget.TextView[contains(@text,"车况")]' )
+            # self.driver.drag_and_drop( e1, e2 )
+            #
+            # uploadPic.uploadpicAgain( self, 2 )
+            #
+            # #运输轨迹
+            # self.driver.find_element_by_id("com.yihu001.kon.driver:id/tv_task_track").click()
+            # sleep(2)
+            # # 截图
+            # self.driver.get_screenshot_as_file( "track.png" )
+            # trackInfo.trackInfo(self)
 
 
         except Exception as e:
